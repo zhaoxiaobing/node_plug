25f2796caa55c8e13d2185d458d8e40def1e511e
Zhao Xiao Bing <378164732@qq.com>
2015-12-01 20:09:54 +0800

    ++

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
 var express = require('express');
 var path = require('path');
+//var dir = process.cwd();
 var fs = require("fs");
+var url = require("url");
 var Handlebars = require("handlebars");
-var url = require("./local.json");
+var favicon = require('serve-favicon');
+var options = require("./local.json");
 
 var app = express();
 
-var htmlUrl = url.html,
-    imagesUrl = url.images,
-    lessUrl = url.less,
-    cssUrl = url.css,
-    jsUrl = url.js;
+var htmlUrls = url.parse(options.url.html),
+    imagesUrls = url.parse(options.url.images),
+    lessUrls = url.parse(options.url.less),
+    cssUrls = url.parse(options.url.css),
+    jsUrls = url.parse(options.url.js);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(imagesUrls.path,express.static(options.mount.images));
+app.use(cssUrls.path,express.static(options.mount.styles));
 
-var data = { "name": "Alan", "hometown": "Somewhere, TX","imagesUrl":imagesUrl,
+
+var data = { "name": "Alan", "hometown": "Somewhere, TX","imagesUrl":options.url.images,
   "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
 
-app.get('/:name',function(req,res) {
+
+/*Handlebars.registerHelper('script',function(value){
+  value = path.resolve(jsUrl,value);
+  console.log(value);
+});*/
+
+app.get(htmlUrls.path + ':name',function(req,res) {
   var name = req.params['name'];
-  html = fs.readFileSync(path.resolve('public/html',name),"utf-8");
-  var result = Handlebars.compile(html)(data);
-  res.send(result);
+  //html = fs.readFileSync(path.join(dir,name),"utf-8");
+  console.log(name);
+  //console.log(path.join(process.cwd(),dir,name));
+  //var result = Handlebars.compile(html)(data);
+  //res.send(result);
 });
 
 
-// catch 404 and forward to error handler
-/*app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});*/
-
-// error handlers
 
 
 
-
+//app.use(favicon(path.resolve(dir, 'public/images/ico/favicon.ico')));
 app.listen(9999);
---
+++ public/js/common.js
@@ -0,0 +1,8 @@
+/**
+ * Created by zhaoxiaobing on 2015/12/1.
+ */
+
+
+exports.test = function(){
+    alert('I am test')
+};
