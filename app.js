45d91634eb815ef277bc9e2045c4cb39932dd6e4
zhaoxiaobing <18611534315@163.com>
2015-12-02 01:45:03 +0800

    plug

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var path = require('path');
-//var dir = process.cwd();
+var dir = process.cwd();
 var fs = require("fs");
 var url = require("url");
 var Handlebars = require("handlebars");
+var browserify = require("browserify");
 var favicon = require('serve-favicon');
+var through2 = require('through2');
 var options = require("./local.json");
 
 var app = express();
@@ -26,18 +28,28 @@ var data = { "name": "Alan", "hometown": "Somewhere, TX","imagesUrl":options.url
   "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
 
 
-/*Handlebars.registerHelper('script',function(value){
-  value = path.resolve(jsUrl,value);
-  console.log(value);
-});*/
+Handlebars.registerHelper('script',function(value){
+  value = path.join(dir,jsUrls.path,value);
+  var b = browserify(value);//.bundle();
+  //b.pipe('./stdout.js');
+  b.transform(function (file) {
+    var data = '';
+    console.log(file);
+    return through(write, end);
+
+    function write (buf) { data += buf }
+    function end () {
+      this.queue(coffee.compile(data));
+      this.queue(null);
+    }
+  });
+});
 
 app.get(htmlUrls.path + ':name',function(req,res) {
   var name = req.params['name'];
-  //html = fs.readFileSync(path.join(dir,name),"utf-8");
-  console.log(name);
-  //console.log(path.join(process.cwd(),dir,name));
-  //var result = Handlebars.compile(html)(data);
-  //res.send(result);
+  html = fs.readFileSync(path.join(dir,htmlUrls.path,name),"utf-8");
+  var result = Handlebars.compile(html)(data);
+  res.send(result);
 });
 
 
