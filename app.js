d11b882b601c6423c49f5e969a8bd9514b284a38
Zhao Xiao Bing <378164732@qq.com>
2015-12-02 20:17:44 +0800

    ++

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var dir = process.cwd();
@@ -6,7 +8,7 @@ var url = require("url");
 var Handlebars = require("handlebars");
 var browserify = require("browserify");
 var favicon = require('serve-favicon');
-var through2 = require('through2');
+var through = require('through');
 var options = require("./local.json");
 
 var app = express();
@@ -21,40 +23,68 @@ var htmlUrls = url.parse(options.url.html),
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(imagesUrls.path,express.static(options.mount.images));
-app.use(cssUrls.path,express.static(options.mount.styles));
+//app.use(cssUrls.path,express.static(options.mount.styles));
+//app.use(jsUrls.path,express.static(options.mount.js));
 
 
 var data = { "name": "Alan", "hometown": "Somewhere, TX","imagesUrl":options.url.images,
   "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
 
 
-Handlebars.registerHelper('script',function(value){
-  value = path.join(dir,jsUrls.path,value);
-  var b = browserify(value);//.bundle();
-  //b.pipe('./stdout.js');
-  b.transform(function (file) {
-    var data = '';
-    console.log(file);
-    return through(write, end);
-
-    function write (buf) { data += buf }
-    function end () {
-      this.queue(coffee.compile(data));
-      this.queue(null);
-    }
-  });
+Handlebars.registerHelper('script',(value)  => {
+  value = jsUrls.href + value;
+  return new Handlebars.SafeString('<script src="'+value+'"></script>');
 });
 
-app.get(htmlUrls.path + ':name',function(req,res) {
-  var name = req.params['name'];
-  html = fs.readFileSync(path.join(dir,htmlUrls.path,name),"utf-8");
-  var result = Handlebars.compile(html)(data);
+
+app.get(htmlUrls.path + ':name',(req,res) => {
+  let name = req.params['name'],
+      html = fs.readFileSync(path.join(dir,htmlUrls.path,name),"utf-8"),
+      result = Handlebars.compile(html)(data);
   res.send(result);
 });
 
 
+app.get(jsUrls.path + ':name',(req,res) =>{
+  var name = req.params['name'],
+      b = browserify(path.resolve(options.mount.js, name), {
+      basedir : dir
+    });
+    res.set('Content-Type', 'application/json');
+    b.transform((f) =>{
+      console.log(1);
+    console.log(f);
+    if (f.match(/\.js$/)) {
+      return through(function (chunk) {
+        this.queue(chunk);
+        this.queue(null);
+      });
+    } else if (f.match(/\.css$/)) {
+      var css = [];
+      return through(function (chunk) {
+        css.push(chunk);
+      }, function () {
+        css = Handlebars.compile(css.join(''))(options).replace(/(\r|\n)/g, '');
+        this.queue(
+            'var style = document.createElement("style");' +
+            'style.innerHTML=\'' + css + '\';' +
+            'document.getElementsByTagName("head")[0].appendChild(style);');
+        this.queue(null);
+      });
+    }
+  });
+    b.bundle((e, buf) => {
+      if (e) {
+      console.error(e);
+      res.sendStatus(500);
+      return;
+    }
+    res.send(buf);
+  });
+});
 
 
 
-//app.use(favicon(path.resolve(dir, 'public/images/ico/favicon.ico')));
+
+app.use(favicon(path.resolve(dir, 'public/images/ico/favicon.ico')));
 app.listen(9999);
